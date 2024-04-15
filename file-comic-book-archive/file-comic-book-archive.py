d4240b4833acdfd79be8db83c6be37057ef0b185
SWEInstructor <70765121+SWEInstructor@users.noreply.github.com>
2024-04-15 18:58:11 -0400

    Updates from midpoint check-in
    Syncing the load and save branches with additional features:
    
    * Exporting multiple layer .cbz files (no layer groups yet)
    * Importing .cbt files
    * Example of how to set up and use a GUI

--- file-comic-book-archive/file-comic-book-archive.py
+++ file-comic-book-archive/file-comic-book-archive.py
@@ -13,7 +13,8 @@ import gi
 
 gi.require_version('Gimp', '3.0')
 from gi.repository import Gimp
-
+gi.require_version('GimpUi', '3.0')
+from gi.repository import GimpUi
 gi.require_version('Gegl', '0.4')
 from gi.repository import Gegl
 from gi.repository import GObject
@@ -27,7 +28,7 @@ import xml.etree.ElementTree as ET
 #TODO: Code is very similar to loading the first image in the CBZ,
 #Could we reduce the code duplication by making a new function that
 #both call?
-def thumbnail_comic_book_archive(procedure, file, thumb_size, args, data):
+def thumbnail_cbz(procedure, file, thumb_size, args, data):
     tempdir = tempfile.mkdtemp('gimp-plugin-comic-book-archive')
     cbaffFile = zipfile.ZipFile(file.peek_path())
 
@@ -67,29 +68,45 @@ def thumbnail_comic_book_archive(procedure, file, thumb_size, args, data):
         GObject.Value(Gimp.Image, img)
     ])
 
+#Wrapper function that passes the Zipfile object to load_image()
+def load_cbz(procedure, run_mode, file, metadata, flags, config, data):
+    compression = zipfile.ZipFile(file.peek_path())
+    return load_image(procedure, run_mode, file, metadata, flags, config, data, compression)
 
-#Function that actually loads the CBZ file
-def load_comic_book_archive(procedure, run_mode, file, metadata, flags, config, data):
-    #Making a temporary folder to unzip the .cbz files to
+#Wrapper function that passes the Tarfile object to load_image()
+def load_cbt(procedure, run_mode, file, metadata, flags, config, data):
+    compression = tarfile.open(file.peek_path(), 'r')
+    return load_image(procedure, run_mode, file, metadata, flags, config, data, compression)
+
+#Function that actually loads the Comic Book Archive file
+def load_image(procedure, run_mode, file, metadata, flags, config, data, compression):
+    #Making a temporary folder to unzip the .cbz/.cbt files to
     tempdir = tempfile.mkdtemp('gimp-plugin-comic-book-archive')
-    #Load the file the user wants to import to the Zipfile object
-    cbaffFile = zipfile.ZipFile(file.peek_path())
 
-    #List of all files in the .cbz file
-    #TODO: Is there a way to sort these so we load the image in order?
-    file_info_list = cbaffFile.infolist()
+    #List of all files in the .cbz/.cbt file
+    # isinstance() is used to determine what file format we're loading
+    if (isinstance(compression, zipfile.ZipFile)):
+        file_info_list = compression.infolist()
+    elif (isinstance(compression, tarfile.TarFile)):
+        file_info_list = compression.getmembers()
     layer_names = []
 
     # Valid file endings
     extensions = ['jpg', 'jpeg', 'png', 'tiff', 'gif', 'bmp']
     # Loop through all files
+    #TODO: Is there a way to sort these so we load the image in order?
     for file_info in file_info_list:
+        filename = ""
+        if (isinstance(compression, zipfile.ZipFile)):
+            filename = file_info.filename
+        elif (isinstance(compression, tarfile.TarFile)):
+            filename = file_info.name
+
         # Check if the end with a valid extension
         # TODO: Verify this actually does filter valid files
-        print(file_info.filename)
-        if file_info.filename.endswith(tuple(extensions)):
+        if filename.endswith(tuple(extensions)):
             # Add the name to a list to use later
-            layer_names.append(file_info.filename)
+            layer_names.append(filename)
         else:
             print("Unsupported layer format")
 
@@ -101,10 +118,14 @@ def load_comic_book_archive(procedure, run_mode, file, metadata, flags, config, 
     for file in layer_names:
         #Create a temporary file to load a layer into
         #TODO: What if it's not a JPEG? Can we make this more flexible?
-        tmp = os.path.join(tempdir, 'tmp.jpeg')
-        with open(tmp, 'wb') as fid:
-            #Write the file from the .cbz to the temporary file
-            fid.write(cbaffFile.read(file))
+
+        if (isinstance(compression, zipfile.ZipFile)):
+            tmp = os.path.join(tempdir, 'tmp.jpeg')
+            with open(tmp, 'wb') as fid:
+                fid.write(compression.read(file))
+        elif (isinstance(compression, tarfile.TarFile)):
+            compression.extract(file, tempdir)
+            tmp = os.path.join(tempdir, file)
 
         #Call the Load Layer function and get the temporary image back as a Layer object
         pdb_proc = Gimp.get_pdb().lookup_procedure('gimp-file-load-layer')
@@ -118,8 +139,12 @@ def load_comic_book_archive(procedure, run_mode, file, metadata, flags, config, 
         result = pdb_proc.run(pdb_config)
 
         layer = result.index(1)
-        #Get the layer's dimensions
-        w, h = layer.get_width(), layer.get_height()
+        #Get the largest layer's dimensions
+        if (w < layer.get_width()):
+            w = layer.get_width()
+        if (h < layer.get_height()):
+            h = layer.get_height()
+
         #Get the layer name from the original image
         layer.set_name(layer_names[index])
         #Add the layer to the image
@@ -141,14 +166,31 @@ def load_comic_book_archive(procedure, run_mode, file, metadata, flags, config, 
         GObject.Value(Gimp.Image, img)
     ]), flags
 
-def save_CBAFF(procedure, run_mode, image, n_drawables, drawables, file, metadata, config, data):
+def save_cbz(procedure, run_mode, image, n_drawables, drawables, file, metadata, config, data):
     def write_file_str(zfile, fname, data):
         zi = zfile.ZipInfo(fname)
         zi.external_attr = int("100644", 8) << 16
         zfile.writestr(zi, data)
 
-    Gimp.progress_init("Exporting CBAFF image")
-    tempdir = tempfile.mkdtemp('gimp-plugin-file-CBAFF')
+    Gimp.progress_init("Exporting Comic Book Archive (cbz) image")
+
+    #GUI Code
+    if run_mode == Gimp.RunMode.INTERACTIVE:
+        GimpUi.init('python-fu-file-cbz-save')
+
+        #Search for this in the repository and see what else you can do
+        dialog = GimpUi.ProcedureDialog(procedure=procedure, config=config)
+        dialog.fill(None)
+        if not dialog.run():
+            dialog.destroy()
+            return procedure.new_return_values(Gimp.PDBStatusType.CANCEL, GLib.Error())
+        else:
+            dialog.destroy()
+
+    #Example of how to access user input
+    print(config.get_property('title'))
+
+    tempdir = tempfile.mkdtemp('gimp-plugin-file-cbz')
     cbaffFile = zipfile.ZipFile(file.peek_path() + '.tmpsave', 'w', compression=zipfile.ZIP_STORED)
 
     def store_layer(image, drawable, path):
@@ -186,43 +228,73 @@ def save_CBAFF(procedure, run_mode, image, n_drawables, drawables, file, metadat
         GObject.Value(Gimp.PDBStatusType, Gimp.PDBStatusType.SUCCESS)
     ])
 class FileComicBookArchive(Gimp.PlugIn):
+    ## Parameters ##
+    __gproperties__ = {
+        "save-metadata": (bool,
+                          ("Save metadata"),
+                          ("Save metadata in ComicInfo format"),
+                          False,
+                          GObject.ParamFlags.READWRITE),
+        "title": (str,
+                 ("Title"),
+                 ("Book Title"),
+                 "",
+                 GObject.ParamFlags.READWRITE),
+    }
+
     ## GimpPlugIn virtual methods ##
     def do_set_i18n(self, procname):
         return True, 'gimp30-python', None
 
     def do_query_procedures(self):
-        return ['file-comic-book-archive-load-thumb',
-                'file-comic-book-archive-load',
-                'file-comic-book-archive-save']
+        return ['file-cbz-thumb',
+                'file-cbz-load',
+                'file-cbt-load',
+                'file-cbz-save']
 
     def do_create_procedure(self, name):
-        if name == 'file-comic-book-archive-load':
+        if name == 'file-cbz-load':
             procedure = Gimp.LoadProcedure.new(self, name,
                                                Gimp.PDBProcType.PLUGIN,
-                                               load_comic_book_archive, None)
+                                               load_cbz, None)
             procedure.set_menu_label('CBZ')
-            procedure.set_documentation('load a Comic Book Archive file',
-                                        'load a Comic Book Archive file',
+            procedure.set_documentation('load a Comic Book Archive (.cbz) file',
+                                        'load a Comic Book Archive (.cbz) file',
                                         name)
             procedure.set_mime_types("application/vnd.comicbook+zip");
             procedure.set_extensions("cbz");
-            procedure.set_thumbnail_loader('file-comic-book-archive-load-thumb');
-        elif name == 'file-comic-book-archive-save':
+            procedure.set_thumbnail_loader('file-cbz-thumb');
+        elif name == 'file-cbt-load':
+            procedure = Gimp.LoadProcedure.new(self, name,
+                                               Gimp.PDBProcType.PLUGIN,
+                                               load_cbt, None)
+            procedure.set_menu_label('CBT')
+            procedure.set_documentation('load a Comic Book Archive (.cbt) file',
+                                        'load a Comic Book Archive (.cbt) file',
+                                        name)
+            procedure.set_mime_types("application/vnd.comicbook+tar");
+            procedure.set_extensions("cbt");
+            #procedure.set_thumbnail_loader('file-cbz-thumb');
+        elif name == 'file-cbz-save':
             procedure = Gimp.SaveProcedure.new(self, name,
                                                Gimp.PDBProcType.PLUGIN,
-                                               False, save_CBAFF, None)
+                                               False, save_cbz, None)
             procedure.set_image_types("*");
-            procedure.set_documentation('save a CBAFF (.cbz) file',
-                                        'save a CBAFF (.cbz) file',
+            procedure.set_documentation('save a Comic Book Archive (.cbz) file',
+                                        'save a Comic Book Archive (.cbz) file',
                                         name)
-            procedure.set_menu_label('CBAFF')
+            procedure.set_menu_label('CBZ')
             procedure.set_extensions("cbz");
-        else:  # 'file-comic-book-archive-load-thumb':
+
+            #Adding parameters for GUI
+            procedure.add_argument_from_property(self, "save-metadata")
+            procedure.add_argument_from_property(self, "title")
+        elif name == 'file-cbz-thumb':
             procedure = Gimp.ThumbnailProcedure.new(self, name,
                                                     Gimp.PDBProcType.PLUGIN,
-                                                    thumbnail_comic_book_archive, None)
-            procedure.set_documentation('loads a thumbnail from a Comic Book Archive file',
-                                        'loads a thumbnail from a Comic Book Archive file',
+                                                    thumbnail_cbz, None)
+            procedure.set_documentation('loads a thumbnail from a Comic Book Archive (.cbz) file',
+                                        'loads a thumbnail from a Comic Book Archive (.cbz) file',
                                         name)
 
             procedure.set_attribution('<YOU>',  # author
@@ -230,6 +302,7 @@ class FileComicBookArchive(Gimp.PlugIn):
                                       '2024')  # year
 
         return procedure
+
 
 
 Gimp.main(FileComicBookArchive.__gtype__, sys.argv)
